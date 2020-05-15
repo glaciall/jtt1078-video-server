5e6cf2f15f857f14838efde7b30cb784b602a5d3
matrixy <matrixy@me.com>
2020-05-15 23:45:35 +0800

    不拷贝音频数据达到静音目的

--- src/main/java/cn/org/hentai/jtt1078/codec/SilenceCodec.java
+++ src/main/java/cn/org/hentai/jtt1078/codec/SilenceCodec.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 /**
  * Created by houcheng on 2019-12-11.
  */
-public class RawDataCopyCodec extends AudioCodec
+public class SilenceCodec extends AudioCodec
 {
+    static final byte[] BLANK = new byte[0];
+
     @Override
     public byte[] toPCM(byte[] data)
     {
-        return Arrays.copyOf(data, data.length);
+        return BLANK;
     }
 
     @Override
     public byte[] fromPCM(byte[] data)
     {
-        return Arrays.copyOf(data, data.length);
+        return BLANK;
     }
 }
