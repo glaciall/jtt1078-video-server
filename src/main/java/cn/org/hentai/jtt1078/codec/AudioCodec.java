87540f56c50995c89175de2877090a9e475e5820
tmyam <tmyam@126.com>
2020-05-20 16:42:13 +0800

    解码器使用

--- src/main/java/cn/org/hentai/jtt1078/codec/AudioCodec.java
+++ src/main/java/cn/org/hentai/jtt1078/codec/AudioCodec.java
@@ -15,6 +15,7 @@ public abstract class AudioCodec
         if (MediaEncoding.Encoding.ADPCMA.ordinal() == encoding) return new ADPCMCodec();
         else if (MediaEncoding.Encoding.G711A.ordinal() == encoding) return new G711Codec();
         else if (MediaEncoding.Encoding.G711U.ordinal() == encoding) return new G711UCodec();
+        else if (MediaEncoding.Encoding.G726.ordinal() == encoding) return new G726Codec();
         // else if (Audio.Encoding.G726.equals(encoding)) ;
         else return new SilenceCodec();
     }
