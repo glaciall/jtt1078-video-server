33fc5e6b9ef15faad77a7ddac0bf2542a552f331
matrixy <matrixy@me.com>
2021-07-26 15:51:19 +0800

    据说改掉了一个G711编码上的一个问题（由群友提供）

--- src/main/java/cn/org/hentai/jtt1078/codec/G711Codec.java
+++ src/main/java/cn/org/hentai/jtt1078/codec/G711Codec.java
@@ -1,9 +1,6 @@
 package cn.org.hentai.jtt1078.codec;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import cn.org.hentai.jtt1078.codec.AudioCodec;
 
 /**
  * 核心转换，PCM转G711
@@ -15,49 +12,87 @@ public class G711Codec extends AudioCodec
     private final static int QUANT_MASK = 0xf;
     private final static int SEG_SHIFT = 4;
     private final static int SEG_MASK = 0x70;
-
     static short[] seg_end = {0xFF, 0x1FF, 0x3FF, 0x7FF, 0xFFF, 0x1FFF, 0x3FFF, 0x7FFF};
+    private final static int cClip = 32635;
+    private static final byte[] aLawCompressTable = new byte[]{1, 1, 2, 2, 3, 3, 3,
+            3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+            6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+            7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+            7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+            7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
 
-    static short search(short val, short[] table, short size)
-    {
+    private static byte linearToALawSample(short sample) {
+        int sign;
+        int exponent;
+        int mantissa;
+        int s;
 
-        for (short i = 0; i < size; i++)
-        {
-            if (val <= table[i])
-            {
+        sign = ((~sample) >> 8) & 0x80;
+        if (!(sign == 0x80)) {
+            sample = (short) -sample;
+        }
+        if (sample > cClip) {
+            sample = cClip;
+        }
+        if (sample >= 256) {
+            exponent = aLawCompressTable[(sample >> 8) & 0x7F];
+            mantissa = (sample >> (exponent + 3)) & 0x0F;
+            s = (exponent << 4) | mantissa;
+        } else {
+            s = sample >> 4;
+        }
+        s ^= (sign ^ 0x55);
+        return (byte) s;
+    }
+
+    /**
+     * PCM转G711
+     *
+     * @param src 编码前的数据
+     * @return 编码后res数组长度应为编码前src数组长度的一半
+     */
+    public static byte[] encode(byte[] src) {
+        int j = 0;
+        int len = src.length;
+        int count = len / 2;
+        byte[] res = new byte[count];
+        short sample = 0;
+        for (int i = 0; i < count; i++) {
+            sample = (short) (((src[j++] & 0xff) | (src[j++]) << 8));
+            res[i] = linearToALawSample(sample);
+        }
+        return res;
+    }
+
+    static short search(short val, short[] table, short size) {
+
+        for (short i = 0; i < size; i++) {
+            if (val <= table[i]) {
                 return i;
             }
         }
         return size;
     }
 
-    public static byte linear2alaw(short pcm_val)
-    {
+    public static byte linear2alaw(short pcm_val) {
         short mask;
         short seg;
         char aval;
-        if (pcm_val >= 0)
-        {
+        if (pcm_val >= 0) {
             mask = 0xD5;
-        }
-        else
-        {
+        } else {
             mask = 0x55;
             pcm_val = (short) (-pcm_val - 1);
-            if (pcm_val < 0)
-            {
-                pcm_val = 32767;
-            }
         }
 
-            /* Convert the scaled magnitude to segment number. */
+        /* Convert the scaled magnitude to segment number. */
         seg = search(pcm_val, seg_end, (short) 8);
 
-            /* Combine the sign, segment, and quantization bits. */
+        /* Combine the sign, segment, and quantization bits. */
 
         if (seg >= 8)       /* out of range, return maximum value. */ return (byte) (0x7F ^ mask);
-        else
-        {
+        else {
             aval = (char) (seg << SEG_SHIFT);
             if (seg < 2) aval |= (pcm_val >> 4) & QUANT_MASK;
             else aval |= (pcm_val >> (seg + 3)) & QUANT_MASK;
@@ -66,8 +101,7 @@ public class G711Codec extends AudioCodec
     }
 
 
-    public static short alaw2linear(byte a_val)
-    {
+    public static short alaw2linear(byte a_val) {
         short t;
         short seg;
 
@@ -75,8 +109,7 @@ public class G711Codec extends AudioCodec
 
         t = (short) ((a_val & QUANT_MASK) << 4);
         seg = (short) ((a_val & SEG_MASK) >> SEG_SHIFT);
-        switch (seg)
-        {
+        switch (seg) {
             case 0:
                 t += 8;
                 break;
@@ -91,72 +124,35 @@ public class G711Codec extends AudioCodec
     }
 
     // 由G.711转至PCM
-    public static byte[] _toPCM(byte[] g711data)
-    {
+    public static byte[] _toPCM(byte[] g711data) {
         byte[] pcmdata = new byte[g711data.length * 2];
-        for (int i = 0, k = 0; i < g711data.length; i++)
-        {
+        for (int i = 0, k = 0; i < g711data.length; i++) {
             short v = alaw2linear(g711data[i]);
             pcmdata[k++] = (byte) (v & 0xff);
-            pcmdata[k++] = (byte)((v >> 8) & 0xff);
+            pcmdata[k++] = (byte) ((v >> 8) & 0xff);
         }
         return pcmdata;
     }
 
     // 由PCM转至G.711
-    public static byte[] _fromPCM(byte[] pcmdata)
-    {
-        byte[] g711data = new byte[pcmdata.length / 2];
-        for (int i = 0, k = 0; i < pcmdata.length; i+=2,k++)
-        {
-            short v = (short)(((pcmdata[i + 1] & 0xff) << 8) | (pcmdata[i] & 0xff));
-            g711data[k] = linear2alaw(v);
-        }
-        return g711data;
+    public static byte[] _fromPCM(byte[] pcmData) {
+        return encode(pcmData);
     }
 
     @Override
-    public byte[] toPCM(byte[] data)
-    {
+    public byte[] toPCM(byte[] data) {
         byte[] temp;
         // 如果前四字节是00 01 52 00，则是海思头，需要去掉
-        if (data[0] == 0x00 && data[1] == 0x01 && (data[2] & 0xff) == (data.length - 4) / 2 && data[3] == 0x00)
-        {
+        if (data[0] == 0x00 && data[1] == 0x01 && (data[2] & 0xff) == (data.length - 4) / 2 && data[3] == 0x00) {
             temp = new byte[data.length - 4];
             System.arraycopy(data, 4, temp, 0, temp.length);
-        }
-        else temp = data;
+        } else temp = data;
 
         return _toPCM(temp);
     }
 
     @Override
-    public byte[] fromPCM(byte[] data)
-    {
-        return _fromPCM(data);
-    }
-
-    public static void main(String[] args) throws Exception
-    {
-        ByteArrayInputStream bais = null;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream(1024 * 1024 * 4);
-
-        int len = -1;
-        byte[] block = new byte[512];
-        FileInputStream fis = new FileInputStream("c:\\users\\houcheng\\downloads\\xxoo.pcm");
-        FileOutputStream fos = new FileOutputStream("d:\\test\\g711\\hahahahahaha.g711");
-
-        while ((len = fis.read(block)) > -1)
-        {
-            baos.write(block, 0, len);
-        }
-
-        byte[] xx = _fromPCM(baos.toByteArray());
-
-        fos.write(xx);
-        fos.flush();
-
-        fis.close();
-        fos.close();
+    public byte[] fromPCM(byte[] data) {
+        return encode(data);
     }
 }
